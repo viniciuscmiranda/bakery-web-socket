050fa67ae5da411cbf2fcbfc7cd643788ed395e7
Vinícius Miranda <viniciusc.miranda@outlook.com>
2025-08-14 00:01:29 -0300

    add hello world

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,8 @@
 import { WebSocketServer } from "./servers/websocket-server";
 import { HttpServer } from "./servers/http-server";
+import { MessageBroker } from "./services/message-broker";
 
 new WebSocketServer(8080).listen();
 new HttpServer(3000).listen();
+
+MessageBroker.send("Hello World!", "startup");
